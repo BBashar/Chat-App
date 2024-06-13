f362abd2053829fb3b336cf0a3d9f266bef320c3
BasharLxrd <laptopbashar@gmail.com>
2024-06-13 09:41:20 +0300

    Did some tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
-
+import SettingsBar from './components/SettingSideBar/settingBar';
+import ContactsList from './components/ContactsSideBar/contacts';
 function App() {
 
   return (
     <>
+      {/* <SettingsBar /> */}
+      <ContactsList />
 
     </>
   )
